a294b462e77cd5de2495e4d2e1f4e9da4d48ffef
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-10 15:59:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// const paintWalls = function() {
+//     console.log("The wall was painted red");
+// };
+
+// paintWalls();
+
+// const paintWalls = function(color) {
+//     console.log("The wall was painted " + color);
+// };
+
+// const color = ["yellow"];
+// paintWalls(color);
+
+const paintWalls = function(wall , color) {
+    console.log("The " + wall + " wall was painted " + color);
+};
+
+const wall = ["north"];
+const color = ["green"];
+paintWalls(wall, color);
